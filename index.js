2489b0eb5060f657fa3d3a655e6fa4e3f036dc2e
tamibolo <122872099+tamibolo@users.noreply.github.com>
2023-05-16 09:00:38 +0200

    changes related to grid

--- index.js
+++ index.js
@@ -1,61 +1,74 @@
 /* PUT YOUR SETUP CODE HERE (requires, initialization, port number, etc.) */
-const express = require('express');
-const app = express();
-const router = express.Router({ mergeParams: true });
-const fs = require('fs');
+const fs = require('node:fs/promises');
+const readFile = fs.readFile;
 const path = require('path');
-const port = 3333;
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+
+
+// This code reads the word list into the wordlist array
+// You can read in enwords.txt for English, frwords.txt for French, or spwords.txt for Spanish
+// You need the 'uft8' argument to get the words as text, not binary
+let wordlist = [];
+let url = 'enwords.txt'
+
+// utility function to read from a file
+async function readFileContent(url) {
+    const data = await readFile(path.join(__dirname, url), { encoding: 'utf8' })
+    wordlist = data.split('\n')     
+}
 
 /* PUT YOUR DEFAULT ROUTE CODE HERE - app.get('/', ... );  */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'))
+})
 
 /* PUT YOUR app.get CODE HERE to return the next or previous set of words in the wordlist array.
-   You may want to return the list of words as a JSON string for easier parsing
-   on the client side. You can do this by using res.json(list) instead of res.send(list)
+    You may want to return the list of words as a JSON string for easier parsing
+    on the client side.  You can do this by using res.json(list) instead of res.send(list)
 */
+// app.get('/prev', (req, res) => {
+//     res.json(wordlist)
+// })
 
-let words = [];
+// app.get('/next', (req, res) => {
+//     res.json(wordlist)
+// })
 
-router.use('/:id', (req, res, next) => {
-  const { id } = req.params;
-  const startIndex = id ? parseInt(id) : 0;
-  const pageSize = 20;
-  
-  const endIndex = startIndex + pageSize;
-  const paginatedWords = words.slice(startIndex, endIndex);
-  
-  res.status(200).json(paginatedWords);
-});
-
-// This code reads the word list into the words array
-// You can read in enwords.txt for English, frwords.txt for French, or spwords.txt for Spanish
-// You need the 'utf8' argument to get the words as text, not binary
-const loadWords = async () => {
-  if (words.length < 1) {
-    try {
-      fs.readFile(path.join(__dirname, 'enwords.txt'), 'utf8', function (err, data) {
-        if (err) {
-          console.error(err);
-          return;
+// Utility route handler function
+async function handler(req, res) {
+     try {
+        const language = req.body.language;
+        console.log(language);
+        if (language === "en") {
+            url = 'enwords.txt'
+        } else if (language === "fr") {
+            url = 'frwords.txt'
+        } else if (language === 'es') {
+            url = 'eswords.txt'
         }
-        
-        words = data.split('\n');
-      });
+
+        await readFileContent(url);
+        res.json(wordlist)
     } catch (error) {
-      console.log(error);
+         console.log(error);
     }
-  }
-  return words;
-};
+}
+
+app.post('/prev', handler);
+
+app.post('/next', handler);
 
 // This is the code that starts the server listening for requests
-loadWords().then(() => {
-  app.use('/', router);
-  
-  let server = app.listen(port, function () {
-    console.log("App server is running on port", port);
-    console.log("To end, press Ctrl + C");
-  });
-  
-  module.exports = server;
+let server = app.listen(PORT, function () {
+    console.log("App server is running on port", PORT);
+    console.log("to end press Ctrl + C");
 });
+
+
+
 
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const ejs = require("ejs")
 const path = require('path') 
 
 const router = express.Router({ mergeParams: true });
-const fs = require('fs');
+const fs = require('node:fs/promises');
+const readFile = fs.readFile;
 
 
 const imageUrls = ["images/ABOUTME.png", "images/HEADSHOT.JPG", "images/INSTAGRAM.png", "images/LINKEDIN.png", "images/doorClosed.png", "images/doorOpened.png"]
@@ -17,39 +18,39 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 const PORT = 8080; 
-let words = [];
 
-router.use('/:id', (req, res, next) => {
-  const { place } = req.body || 0;
-  const startIndex = id ? parseInt(place) : 0;
-  const pageSize = 20;
-  
-  const endIndex = startIndex + pageSize;
-  const paginatedWords = words.slice(startIndex, endIndex);
-  
-  res.render('index.ejs', {
-    words: paginatedWords,
-    place: place + 20
-  })
-});
+let wordlist = [];
+let url = 'enwords.txt'
 
-const loadWords = async () => {
-  if (words.length < 1) {
-    try {
-      fs.readFile(path.join(__dirname, 'enwords.txt'), 'utf8', function (err, data) {
-        if (err) {
-          console.error(err);
-          return;
+
+async function readFileContent(url) {
+    const data = await readFile(path.join(__dirname, url), { encoding: 'utf8' })
+    wordlist = data.split('\n')     
+}
+
+// Utility route handler function
+async function handler(req, res) {
+     try {
+        const language = req.body.language;
+        console.log(language);
+        if (language === "en") {
+            url = 'enwords.txt'
+        } else if (language === "fr") {
+            url = 'frwords.txt'
+        } else if (language === 'es') {
+            url = 'eswords.txt'
         }
-        
-        words = data.split('\n');
-      });
+
+        await readFileContent(url);
+        res.json(wordlist)
     } catch (error) {
-      console.log(error);
+         console.log(error);
     }
-  }
-  return words;
-};
+}
+
+app.post('/prev', handler);
+
+app.post('/next', handler);
 
 
 app.set('view engine', "ejs")
@@ -57,42 +58,32 @@ app.set('view engine', "ejs")
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-  const { id } = req.params || 0;
-  const startIndex = id ? parseInt(id) : 0;
-  const pageSize = 20;
-  
-  const endIndex = startIndex + pageSize;
-  const paginatedWords = words.slice(startIndex, endIndex);
-  
-  res.render('index.ejs', {
-    words: paginatedWords,
-    place: id + 20
-  })
+  res.render('index.ejs')
 });
 
-// app.get('/aboutme', (req, res) => {
-//   res.render("aboutme")
-// });
+app.get('/aboutme', (req, res) => {
+  res.render("aboutme")
+});
 
-// app.get('/books', (req, res) => {
-//   res.render("books")
-// });
+app.get('/books', (req, res) => {
+  res.render("books")
+});
 
-// app.get('/websites', (req, res) => {
-//   res.render("websites")
-// });
+app.get('/websites', (req, res) => {
+  res.render("websites")
+});
 
-// app.get('/random', (req, res) => {
-//   res.render("random")
-// });
+app.get('/random', (req, res) => {
+  res.render("random")
+});
 
-// app.get('/random/generate', (req, res) => {
-//   res.json({url: generateRandomUrl()})
-// });
+app.get('/random/generate', (req, res) => {
+  res.json({url: generateRandomUrl()})
+});
 
 
 
-loadWords().then(() => {
+
   app.use('/', router);
   
   app.listen(PORT, () => {
@@ -100,4 +91,4 @@ loadWords().then(() => {
     console.log("to end press Ctrl + C");
   });
   
-});
+
